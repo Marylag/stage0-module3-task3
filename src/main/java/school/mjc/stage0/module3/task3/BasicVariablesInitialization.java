de1b1b19db3e2b67095d8fa965b0d43ce349f5fb
mary <laguta113@gmail.com>
2023-11-19 11:15:04 +0600

    Commit with a solution

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int one = 1;
+        int two = 10;
+        int three = 100;
+
+        System.out.println(one + "\n" + two + "\n" + three);
     }
 }
